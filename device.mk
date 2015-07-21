0ba9916d81433a8cbbfc208924b1f993eaf8cda9
HolyAngel <slverwolf@gmail.com>
2015-07-21 12:40:54 -0500

    Bringup changes from purenexus

--- device.mk
+++ device.mk
@@ -34,7 +34,6 @@ PRODUCT_COPY_FILES += \
     device/moto/shamu/init.shamu.rc:root/init.shamu.rc \
     device/moto/shamu/init.shamu.power.rc:root/init.shamu.power.rc \
     device/moto/shamu/init.shamu.usb.rc:root/init.shamu.usb.rc \
-    device/moto/shamu/init.performance_profiles.rc:root/init.performance_profiles.rc \
     device/moto/shamu/fstab.shamu:root/fstab.shamu \
     device/moto/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc
 
@@ -106,6 +105,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/moto/shamu/spn-conf.xml:system/etc/spn-conf.xml
 
+PRODUCT_TAGS += dalvik.gc.type-precise
+
 # This device is 560dpi.  However the platform doesn't
 # currently contain all of the bitmaps at 560dpi density so
 # we do this little trick to fall back to the xxhdpi version
@@ -162,29 +163,12 @@ PRODUCT_PACKAGES += \
     libaudio-resampler
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.audio.monitorRotation=true 
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    fmas.spkr_6ch=35,20,110 \
-    fmas.spkr_2ch=35,25 \
-    fmas.spkr_angles=10 \
-    fmas.spkr_sgain=0 \
     media.aac_51_output_enabled=true \
     persist.audio.dualmic.config=endfire \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=false \
-    persist.radio.sib16_support=1 \
-    persist.rcs.supported=0
-
-# never dexopt the MotoSignature
-$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
-
-# WiFi calling
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.iwlan.enable=true \
-    persist.radio.ignore_ims_wlan=1 \
-    persist.radio.data_con_rprt=1
+    ro.audio.monitorRotation=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.isUsbOtgEnabled=1
@@ -209,9 +193,6 @@ PRODUCT_PACKAGES += \
     mm-jpeg-interface-test \
     mm-qcamera-app
 
-# never dexopt the MotoSignature
-$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
-
 PRODUCT_PACKAGES += \
     libion
 
@@ -227,8 +208,8 @@ PRODUCT_PACKAGES += \
     charger_res_images
 
 # for launcher layout
-PRODUCT_PACKAGES += \
-    ShamuLayout
+#PRODUCT_PACKAGES += \
+#    ShamuLayout
 
 PRODUCT_PACKAGES += \
     bdAddrLoader
@@ -253,11 +234,21 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.no_wait_for_card=1
+    persist.radio.no_wait_for_card=1 \
+    persist.radio.sib16_support=1
 
-# Include IMSEnabler
-PRODUCT_PACKAGES += \
-    IMSEnabler
+# never dexopt the MotoSignature
+$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
+
+# WiFi calling
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.radio.ignore_ims_wlan=1 \
+    persist.radio.data_con_rprt=1
+
+# Rich Communications Service is disabled in 5.1
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rcs.supported=0
 
 #Reduce IMS logging
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -277,7 +268,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # LTE, CDMA, GSM/WCDMA
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10 \
-    ro.telephony.get_imsi_from_sim=true \
     telephony.lteOnCdmaDevice=1
 
 # SIM based FSG loading & MCFG activation
@@ -320,7 +310,6 @@ PRODUCT_PACKAGES += \
 
 # NFC packages
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
     nfc_nci.bcm2079x.default \
     NfcNci \
     Tag
@@ -330,6 +319,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     device/moto/shamu/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/moto/shamu/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf
+
+# NFCEE access control
+PRODUCT_COPY_FILES += \
+    device/moto/shamu/nfcee_access.xml:system/etc/nfcee_access.xml
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
@@ -353,11 +346,25 @@ endif
 # Enable for volte call
 AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
 
-# configure the HWUI memory limits
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.hwui.texture_cache_size=72 \
+   ro.hwui.layer_cache_size=48 \
+   ro.hwui.r_buffer_cache_size=8 \
+   ro.hwui.path_cache_size=32 \
+   ro.hwui.gradient_cache_size=1 \
+   ro.hwui.drop_shadow_cache_size=6 \
+   ro.hwui.texture_cache_flushrate=0.4 \
+   ro.hwui.text_small_cache_width=1024 \
+   ro.hwui.text_small_cache_height=1024 \
+   ro.hwui.text_large_cache_width=2048 \
+   ro.hwui.text_large_cache_height=1024
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+   dalvik.vm.heapgrowthlimit=256m
 
 # setup dalvik vm configs.
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, hardware/qcom/msm8x84/msm8x84.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x84/msm8x84-gpu-vendor.mk)
--- power/power_shamu.c
+++ power/power_shamu.c
@@ -30,7 +30,6 @@
 #include <stdbool.h>
 
 #define LOG_TAG "PowerHAL"
-#define LOG_NDEBUG 0
 #include <utils/Log.h>
 
 #include <hardware/hardware.h>
@@ -88,7 +87,7 @@ static void coresonline(int off)
     }
 
     if (rc < 0) {
-        //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
     }
 }
 
@@ -116,7 +115,7 @@ static void enc_boost(int off)
     }
 
     if (rc < 0) {
-        //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
     }
 }
 
@@ -167,7 +166,7 @@ static void touch_boost()
     rc = sendto(client_sockfd, data, strlen(data), 0,
         (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
     if (rc < 0) {
-        //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
     }
 }
 
@@ -188,13 +187,13 @@ static void low_power(int on)
         snprintf(data, MAX_LENGTH, "10:%d", client);
         rc = sendto(client_sockfd, data, strlen(data), 0, (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
         if (rc < 0) {
-            //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+            ALOGE("%s: failed to send: %s", __func__, strerror(errno));
         }
     } else {
         snprintf(data, MAX_LENGTH, "9:%d", client);
         rc = sendto(client_sockfd, data, strlen(data), 0, (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
         if (rc < 0) {
-            //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+            ALOGE("%s: failed to send: %s", __func__, strerror(errno));
         }
     }
 }
@@ -236,7 +235,7 @@ static void power_hint( __attribute__((unused)) struct power_module *module,
 {
     switch (hint) {
         case POWER_HINT_INTERACTION:
-            //ALOGV("POWER_HINT_INTERACTION");
+            ALOGV("POWER_HINT_INTERACTION");
             touch_boost();
             break;
 #if 0
